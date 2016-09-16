c5a2f7afdd2b63e0399320d308dd15d74aafe1f5
Stefan Bejan <stefan.bejan90@gmail.com>
2016-09-16 14:01:02 +0200

    Removed class files from solution

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -1,4 +1,9 @@
 package Client;
 
 public class ClientMain {
+
+    public static void main(String[] args) {
+        System.out.print("Starting the Black Jack Client application...");
+    }
+
 }
--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -1,4 +1,8 @@
 package Server;
 
 public class ServerMain {
+
+    public static void main(String[] args) {
+        System.out.print("Starting the Black Jack Server application...");
+    }
 }
